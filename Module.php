54b849b3691dc89fe39dd962fc433e4bd8b5c486
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-08-26 21:58:57 +0100

    composer and reset

--- Module.php
+++ Module.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace ZendSkeletonModule;
+namespace G403Translator;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
