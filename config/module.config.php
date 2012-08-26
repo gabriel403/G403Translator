29851ade77f347ff9b708d0db0fd829cc9dd87d7
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-08-26 22:14:39 +0100

    config

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,19 @@
 <?php
 return array(
+    'service_manager' => array(
+        'factories' => array(
+            'translator'            => 'Zend\I18n\Translator\DatabaseTranslatorServiceFactory',
+        ),
+    ),
+    'translator' => array(
+        'locale' => 'en_US',
+        'translation_db' => array(
+            array(
+                'type'                  => 'database',
+                'dbconnection'          => 'Zend\Db\Adapter\Adapter',
+                'locale_table_name'     =>'locale',
+                'messages_table_name'   =>'message'
+            ),
+        ),
+    ),
 );
